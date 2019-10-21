05b1c60f49037e2fcbc6430fba6b1153372be1a1
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2019-10-21 14:26:10 +0200

    rename example group to "recalculate partial content disabled"

--- spec/filters/s3_access_log_spec.rb
+++ spec/filters/s3_access_log_spec.rb
@@ -153,7 +153,7 @@ shared_examples "recalculate partial content enabled" do
 
 end
 
-shared_examples "don't recalculate partial content" do
+shared_examples "recalculate partial content disabled" do
 
   sample %(2cf7e6b06335c0689c6d29163df5bb001c96870cd78609e3845f1ed76a632621 media.kerkdienstgemist.nl [02/Oct/2010:18:29:16 +0000] 82.168.113.55 2cf7e6b06335c0689c6d29163df5bb001c96870cd78609e3845f1ed76a632621 4F911681022807C6 REST.GET.OBJECT 10028050/2010-09-26-1830.mp3 "GET /10028050/2010-09-26-1830.mp3?Signature=E3ehd6nkXjNg7vr%2F4b3LtxCWads%3D&Expires=1286051333&AWSAccessKeyId=AKIAI3XHXJPFSJW2UQAQ HTTP/1.1" 206 - 4194304 17537676 1600 12 "-" "VLC media player - version 1.0.5 Goldeneye - (c) 1996-2010 the VideoLAN team" -) do
     insist { subject["message"] } == "82.168.113.55 - 2cf7e6b063 [02/Oct/2010:18:29:16 +0000] \"GET /10028050/2010-09-26-1830.mp3?Signature=E3ehd6nkXjNg7vr%2F4b3LtxCWads%3D&Expires=1286051333&AWSAccessKeyId=AKIAI3XHXJPFSJW2UQAQ HTTP/1.1\" 206 4194304 \"-\" \"VLC media player - version 1.0.5 Goldeneye - (c) 1996-2010 the VideoLAN team\" 2"
@@ -282,7 +282,7 @@ describe "Custom s3_access_log filter solution" do
         }
       }
     )
-    it_behaves_like "don't recalculate partial content"
+    it_behaves_like "recalculate partial content disabled"
   end
 
   describe "with copy_operation set to 'drop'" do
