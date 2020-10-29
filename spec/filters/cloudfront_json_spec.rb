63807cf645e2b0b3648899214ce8d2bf8be98e3c
Klaas Jan Wierenga <k.j.wierenga@gmail.com>
2020-10-29 15:31:00 +0100

    indent JSON and use meaningful heredoc label

--- spec/filters/cloudfront_json_spec.rb
+++ spec/filters/cloudfront_json_spec.rb
@@ -26,42 +26,42 @@ def validate_cloudfront_json_fields
   # insist { subject['httpversion']        } != nil
 end
 
-JSON_SAMPLE = JSON.parse(<<EOF
-{
-  "_index": "cloudfront_sessions-20201026",
-  "_type": "_doc",
-  "_id": "24.132.188.152|+|GET|+|/vodcdn/_definst_/mp4:amazons3/media.kerkdienstgemist.nl/90311151-v1520373/20201010085600_15620922-mp4.mp4|+|HTTP/2.0|+|200...",
-  "_score": 1,
-  "_source": {
-    "session_start": "2020-10-26T00:13:53.000+0000",
-    "timestamp": "2020-10-26T00:14:16.000+0000",
-    "clientip": "24.132.188.152",
-    "verb": "GET",
-    "request": "/vodcdn/_definst_/mp4:amazons3/media.kerkdienstgemist.nl/90311151-v1520373/20201010085600_15620922-mp4.mp4",
-    "protocol": "HTTP/2.0",
-    "response": "200",
-    "agent": [
-      "AppleCoreMedia/1.0.0.16G201 (iPad; U; CPU OS 12_4_8 like Mac OS X; nl_nl)"
-    ],
-    "referer": [
-      "https://kerkdienstgemist.nl/stations/1419/events/recording/160232040001419"
-    ],
-    "session_id": "24.132.188.152#GET#/vodcdn/_definst_/mp4:amazons3/media.kerkdienstgemist.nl/90311151-v1520373/20201010085600_15620922-mp4.mp4#HTTP/2.0#200#1603671233000#1603671256000",
-    "bytes": 42048967,
-    "duration": 23,
-    "logsource": [
-      "AMS50-C1"
-    ],
-    "kbps": 14282,
-    "hls_files": {
-      "playlist": 1,
-      "chunklist": 1,
-      "media": 16
-    },
-    "count": 18
+JSON_SAMPLE = JSON.parse(<<JSON_DOC
+  {
+    "_index": "cloudfront_sessions-20201026",
+    "_type": "_doc",
+    "_id": "24.132.188.152|+|GET|+|/vodcdn/_definst_/mp4:amazons3/media.kerkdienstgemist.nl/90311151-v1520373/20201010085600_15620922-mp4.mp4|+|HTTP/2.0|+|200...",
+    "_score": 1,
+    "_source": {
+      "session_start": "2020-10-26T00:13:53.000+0000",
+      "timestamp": "2020-10-26T00:14:16.000+0000",
+      "clientip": "24.132.188.152",
+      "verb": "GET",
+      "request": "/vodcdn/_definst_/mp4:amazons3/media.kerkdienstgemist.nl/90311151-v1520373/20201010085600_15620922-mp4.mp4",
+      "protocol": "HTTP/2.0",
+      "response": "200",
+      "agent": [
+        "AppleCoreMedia/1.0.0.16G201 (iPad; U; CPU OS 12_4_8 like Mac OS X; nl_nl)"
+      ],
+      "referer": [
+        "https://kerkdienstgemist.nl/stations/1419/events/recording/160232040001419"
+      ],
+      "session_id": "24.132.188.152#GET#/vodcdn/_definst_/mp4:amazons3/media.kerkdienstgemist.nl/90311151-v1520373/20201010085600_15620922-mp4.mp4#HTTP/2.0#200#1603671233000#1603671256000",
+      "bytes": 42048967,
+      "duration": 23,
+      "logsource": [
+        "AMS50-C1"
+      ],
+      "kbps": 14282,
+      "hls_files": {
+        "playlist": 1,
+        "chunklist": 1,
+        "media": 16
+      },
+      "count": 18
+    }
   }
-}
-EOF
+JSON_DOC
 )
 
 shared_examples "adds a fingerprint" do
